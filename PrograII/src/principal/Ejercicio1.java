8c201a9003d217330b885128b7af2b3f0a7122f3
Matias Saffirio <saffiriomatias@gmail.com>
2021-04-24 15:29:35 -0300

    cambios

--- PrograII/src/principal/Ejercicio1.java
+++ PrograII/src/principal/Ejercicio1.java
@@ -20,18 +20,35 @@ public class Ejercicio1 {
         //Cargo la pila 2
         p2.Apilar(1);
         p2.Apilar(2);
-        p2.Apilar(3);
+        p2.Apilar(1);
+
+        /*
+        Pila 1
+        4
+        3
+        2
+        1
+        0
+
+        Pila 2
+        1
+        2
+        1
+        */
         int aux;
         while (!p2.PilaVacia()){
             aux = p1.Tope();
             if(aux == p2.Tope()){
-                System.out.println("p1 " + aux +" p2 " + p2.Tope());
+                //System.out.println("p1 " + aux +" p2 " + p2.Tope());
                 p2.Desapilar();
+                p1.Desapilar();
+            } else  {
+                p1.Desapilar();
             }
-            p1.Desapilar();
+
 
         }
-        return false;
+        return true;
     }
 
 }
