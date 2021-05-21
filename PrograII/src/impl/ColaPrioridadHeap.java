50d003cf1d54dc6dd3e210621005b3d947e71759
msaffirio <saffiriomatias@gmail.com>
2021-05-21 19:08:24 -0300

    creación de ColaPrioridadHeap

---
+++ PrograII/src/impl/ColaPrioridadHeap.java
@@ -0,0 +1,30 @@
+package impl;
+
+import api.ColaTDA;
+
+public class ColaPrioridadHeap implements ColaTDA {
+    @Override
+    public void IncicializarCola() {
+
+    }
+
+    @Override
+    public void Acolar(int x) {
+
+    }
+
+    @Override
+    public void Desacolar() {
+
+    }
+
+    @Override
+    public boolean ColaVacia() {
+        return false;
+    }
+
+    @Override
+    public int Primero() {
+        return 0;
+    }
+}
